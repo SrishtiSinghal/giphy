86761e8c73623d833261461ac63e4feaabcb5203
Srishti Singhal <singhalsrishti98@gmail.com>
2019-12-30 13:40:10 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const Giphy_api_key = "sFulW8b5Fb2gH06SnuUMs0HccAOlbXuG";
+let currentText = "";
 
 function App() {
+  const [imageURLs, setImageURLs] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  /*
+  useEffect(function() {
+    getGiphyURL(setImageURL);
+  }, []);
+*/
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h3> Search Gifs </h3>
+      <form>
+        <input
+          type="text"
+          value={searchText}
+          onChange={function(event) {
+            const text = event.target.value;
+
+            currentText = text;
+            setSearchText(text);
+            getGiphyURL(text, setImageURLs);
+          }}
+          placeholder="Start typing here"
+        />
+      </form>
+      {imageURLs.map(arrayElement => (
+        <img src={arrayElement} />
+      ))}
     </div>
   );
+}
+
+async function getGiphyURL(searchText, setImageURLs) {
+  if (searchText === "") {
+    setImageURLs([]);
+    return;
+  }
+  const url =
+    "https://api.giphy.com/v1/gifs/search?&limit=10&q=" +
+    searchText +
+    "&api_key=" +
+    Giphy_api_key;
+  const response = await fetch(url);
+  const parsedResponse = await response.json();
+  console.log(parsedResponse);
+
+  const data = parsedResponse.data;
+
+  if (searchText != currentText) {
+    //to deal with the delay caused by async call
+    return;
+  }
+  const urls = data.map(function(arrayElement) {
+    return arrayElement.images.original.url;
+  });
+  setImageURLs(urls);
 }
 
 export default App;
